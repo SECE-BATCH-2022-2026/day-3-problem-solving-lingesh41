187065f92c408a2f8dae324307d0453b0e927001
lingesh <lingeshwar0410@gmail.com>
2025-12-05 14:24:53 +0530

    Adding pg2

---
+++ pg2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class pg2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+	int ans = pg2.su(a, b, c);
+        System.out.println(ans);
+    }
+    public static int su(int a, int b, int c) {
+        if (a == 13) {
+            if (c == 13) {
+                return 0;
+            } else {
+                return c;
+            }
+        }
+        else if (b == 13) {
+            return a;
+        }
+        else if (c == 13) {
+            return a + b;
+        }
+        else {
+            return a + b + c;
+        }
+    }
+}
