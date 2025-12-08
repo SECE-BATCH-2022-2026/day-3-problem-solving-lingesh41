853e033947ac467563b184772f320ff8b8f2b476
lingesh <lingeshwar0410@gmail.com>
2025-12-08 15:30:55 +0530

    Adding pg4

---
+++ pg4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pg4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	String input = sc.nextLine();
+	boolean result = UserMainCode.validateCode(input);
+	System.out.println(result);
+    }
+}
+
+class UserMainCode {
+	public static boolean validateCode(String s) {
+        	String pattern = "^CPT-\\d{6}$";
+        	return s.matches(pattern);
+    	}
+}
